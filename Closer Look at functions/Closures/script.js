0d833b406acb26750bde8cd9d6e42cb52d7e5100
RehmanAli7 <rehman@ncryptron.com>
2026-01-01 10:50:24 +0500

    added how browser web apis handle events and explained with example

--- Closer Look at functions/Closures/script.js
+++ Closer Look at functions/Closures/script.js
@@ -41,19 +41,96 @@ How closure works
 ---------------
 booker is the same function which is located in global scope also. when booker function is called first thing thats gonna happen is a new excution context is gonna be created and put on top of call stack and the variable enviornment of this context is empty simply because there are no variables declared 
 in this function now what about the scope chain? well since booker is in the global execution context its simply a child's scope of the global scope. So, how did the booker function access the passengerCount variable? as it is no where to be found in the scope chain so this where we start to unvail the secret of the 
-closure and the secret is basically this any function always has access vaiables to the variable enviornment of the execution context in which the function was created.    
+closure and the secret is basically this any function always has access to the variable enviornment of the execution context in which the function was created. Now in the case of booker this function was created. It was born in the execution context of secureBooking, which was popped of the stack previously. So, therefore the 
+the booker function will get access to this variable enviornemnt which contains the passengerCount variable. And this is how the function will be able to read and manipulate passengerCountVariable. So it is this connection that we call closure.
+
+--> So a function always has access to the variable enviornment of the execution context in which it was created, even after that execution context is gone.
+
+--> So the booker function has access to the passengerCount variable becuase its basically defined in the scope in which the booker function was actually created. So, in a sense the scope chain is actually preserved through the closure, even when a scope has already been destroyed becuase the execution context is gone. This means that 
+even though the execution context has actually been destroyed, the variable enviornment somehow keeps living somewhere in the engine.
+
+--> We can also say that thanks to the closure a function does not lose connection to variables that are existed at the function birthplace.
+
+--> Note: Closure have priority over scope chain.
+
+Closures Summary
+---------------
+--> Closure is the closed over variable enviornment of the execution context in which a function was created even after the execution context is gone.
+
+--> A closure gives a function access to all the variables of its parent function. So the function in which it is defined even after that parent function has returned. So the function kees the refrence to outer scope even after that outer scope is gone which basically preserves the scope chain throughout time.
+
+--> A closure makes sure that a function does never lose connection to the variables that existed at the functions birthplace. It remembers the variable even after the birthplace is gone. It's like a person who doesn't lose connection to their hometown. In this analogy person is the function and the hometown is the function parents scope, and the function then 
+doesn't lose the connection to the variables stored in this parent's scope. 
+
+--> A closure is like a backpack so in this analogy, a function has a backpack which it carries around wherever it goes. And this backpack contains all the variables that were present in the enviornment in which the function was created. Then whenever a variable can't be found in the function scope, javascript will look in to the backpack and take the missing variables 
+from there.
+
+--> we can took a look in to closures using console.dir() e.g console.dir(booker)
+
 */
 
-const secureBooking = function () {
-  let passengerCount = 0;
+// const secureBooking = function () {
+//   let passengerCount = 0;
 
-  return function () {
-    passengerCount++;
-    console.log(`${passengerCount} passengers`);
-  };
+//   return function () {
+//     passengerCount++;
+//     console.log(`${passengerCount} passengers`);
+//   };
+// };
+
+// const booker = secureBooking();
+// booker();
+// booker();
+// booker();
+
+///////////////////////////////////////////////////////////
+/*   
+Closure Examples
+---------------
+--> These example are gonna demonstrate that we dont't need to return a function from another function in order to create a closure. 
+
+--> Example 1: So whenever something like this happens where you reassigned functions even without returning them, keep in mind that also this will create a closure.
+
+--> Example 2: Closure even have priority over scope chain
+*/
+/////////////////////////////////////////////////////////
+
+// Example 1:
+// let f;
+
+// const g = function () {
+//   const a = 23;
+//   f = function () {
+//     console.log(a * 2);
+//   };
+// };
+
+// const h = function () {
+//   const b = 777;
+//   f = function () {
+//     console.log(b * 2);
+//   };
+// };
+
+// g();
+// f();
+// console.dir(f);
+
+// // Re-assigning f function
+// h();
+// f();
+// console.dir(f);
+
+// Example 2
+const boardPassengers = function (n, wait) {
+  const perGroup = n / 3;
+
+  setTimeout(function () {
+    console.log(`we are now boarding all ${n} passengers`);
+    console.log(`There are three groups each with ${perGroup} passengers`);
+  }, wait * 1000);
+
+  console.log(`Will start boarding in ${wait} seconds`);
 };
-
-const booker = secureBooking();
-booker();
-booker();
-booker();
+const perGroup = 1000;
+boardPassengers(180, 3);
