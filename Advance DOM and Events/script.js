7f5b191cd025dd6c418de81cce5f680ffc3fb80b
RehmanAli7 <rehman@ncryptron.com>
2026-01-04 03:28:15 +0500

    added logic to handle smooth scroll to sections

--- Advance DOM and Events/script.js
+++ Advance DOM and Events/script.js
@@ -7,6 +7,10 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const navLinks = document.querySelectorAll('.nav__link');
+const nav = document.querySelector('.nav');
+const sections = document.querySelectorAll('.section');
+const header = document.querySelector('.header');
 
 const openModal = function () {
   modal.classList.remove('hidden');
@@ -29,3 +33,46 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+console.log('navLinks', navLinks);
+navLinks.forEach(function (navLi) {
+  navLi.addEventListener('click', function (event) {
+    event.preventDefault();
+    const targetId = navLi.getAttribute('href');
+    const targetSection = document.querySelector(targetId);
+
+    console.log('targetSection', targetSection);
+
+    if (targetSection) {
+      targetSection.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+      });
+
+      // add class nav sticky to nav element
+      nav.classList.add('sticky');
+    }
+  });
+});
+
+// window.addEventListener('scroll', function () {
+//   if (this.window.scrollY >= header.offsetHeight) {
+//     nav.classList.add('sticky');
+//   } else {
+//     nav.classList.remove('sticky');
+//   }
+// });
+
+const observer = new IntersectionObserver(function (entries) {
+  console.log('entries', entries);
+  const [entry] = entries;
+
+  console.log('isIntersecting', entry.isIntersecting);
+  if (!entry.isIntersecting) {
+    nav.classList.add('sticky');
+  } else {
+    nav.classList.remove('sticky');
+  }
+});
+
+observer.observe(header);
